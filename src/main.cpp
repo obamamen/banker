2add772d631fb782bab7780fa522764e9a07862a
obamamen <Moosmegens@gmail.com>
2025-11-03 16:45:35 +0100

    working local handshake using monocypher lib

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,52 @@
 #include "../include/banker/networker/client.hpp"
 #include "banker/networker/server.hpp"
 
+#include "banker/monocypher/monocypher.h"
+#include "banker/monocypher/monocypher-ed25519.h"
+
 using namespace banker;
+
+void test_handshake()
+{
+    uint8_t alice_sk[32];
+    uint8_t alice_pk[32];
+    uint8_t bob_sk[32];
+    uint8_t bob_pk[32];
+
+    uint8_t shared_a[32];
+    uint8_t shared_b[32];
+
+    uint8_t final_key_a[32];
+    uint8_t final_key_b[32];
+
+    for (int i = 0; i < 32; i++)
+    {
+        alice_sk[i] = rand() & 0xFF;
+        bob_sk[i]   = rand() & 0xFF;
+    }
+
+    crypto_x25519_public_key(alice_pk, alice_sk);
+    crypto_x25519_public_key(bob_pk, bob_sk);
+
+    crypto_x25519(shared_a, alice_sk, bob_pk);
+    crypto_x25519(shared_b, bob_sk, alice_pk);
+
+    crypto_blake2b(final_key_a, sizeof(final_key_a), shared_a, sizeof(shared_a));
+    crypto_blake2b(final_key_b, sizeof(final_key_b), shared_b, sizeof(shared_b));
+
+    printf("Alice key: ");
+    for (int i = 0; i < 32; i++) printf("%02x", final_key_a[i]);
+    printf("\n");
+
+    printf("Bob key:   ");
+    for (int i = 0; i < 32; i++) printf("%02x", final_key_b[i]);
+    printf("\n");
+
+    if (memcmp(final_key_a, final_key_b, 32) == 0)
+        printf("Handshake SUCCESS! Keys match.\n");
+    else
+        printf("Handshake FAILED! Keys differ.\n");
+}
 
 void server()
 {
@@ -20,6 +65,7 @@ void server()
 
 void client()
 {
+    test_handshake();
     networker::client client = {"127.0.0.1", 8080};
 }
 
