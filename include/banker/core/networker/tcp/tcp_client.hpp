e598ee042d40c90cc5e5cfd1cd931aabd8adab02
obamamen <Moosmegens@gmail.com>
2025-11-18 11:35:12 +0100

    working on extra layer

---
+++ include/banker/core/networker/tcp/tcp_client.hpp
@@ -0,0 +1,19 @@
+/* ================================== *\
+ @file     tcp_client.hpp
+ @project  banker
+ @author   moosm
+ @date     11/18/2025
+*\ ================================== */
+
+#ifndef BANKER_TCP_CLIENT_HPP
+#define BANKER_TCP_CLIENT_HPP
+
+namespace banker::networker
+{
+    class tcp_client
+    {
+
+    };
+}
+
+#endif //BANKER_TCP_CLIENT_HPP
